2f1aca7981bfceb12e61dbbd1078015533ea6275
gatheluck <gatheluck+tech@gmail.com>
2020-09-21 04:36:56 +0900

    add function save_first_layer_weight_freq

--- libs/first_layer.py
+++ libs/first_layer.py
@@ -2,11 +2,12 @@ import logging
 import os
 import sys
 
-import torch
 import torchvision
 
 base = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../")
 sys.path.append(base)
+
+from fft import fft_shift
 
 
 def extract_target_modules(
@@ -56,6 +57,43 @@ def save_first_layer_weight(
         )
 
 
+def save_first_layer_weight_freq(
+    model, log_path: str, bias: int = 0.5, is_orator: bool = True, **kwargs
+) -> None:
+    model.eval()
+    os.makedirs(os.path.dirname(log_path), exist_ok=True)
+
+    conv2d_modules = extract_target_modules(model, target_module_name="torch.nn.Conv2d")
+    first_conv2d_weight = conv2d_modules[0].weight + bias
+    first_conv2d_weight = first_conv2d_weight.clamp(min=0.0, max=1.0)
+
+    freq_image = first_conv2d_weight.rfft(2, normalized=True, onesided=False)
+    freq_image_shifted_rgb = fft_shift(freq_image).norm(
+        dim=-1
+    )  # normalize complex number: [b, c, h, w]
+
+    freq_image_shifted_gray = freq_image_shifted_rgb.sum(dim=-3, keepdim=True)
+
+    torchvision.utils.save_image(
+        freq_image_shifted_rgb,
+        os.path.splitext(log_path)[0] + "_rgb" + os.path.splitext(log_path)[1],
+        padding=1,
+    )
+    torchvision.utils.save_image(
+        freq_image_shifted_gray,
+        os.path.splitext(log_path)[0] + "_gray" + os.path.splitext(log_path)[1],
+        padding=1,
+    )
+
+    # log info
+    if is_orator:
+        logging.info(
+            "save_first_layer_weight_freq: images are saved under [{log_dir}]".format(
+                log_dir=os.path.dirname(log_path)
+            )
+        )
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     model = torchvision.models.resnet50(pretrained=True)
@@ -65,3 +103,6 @@ if __name__ == "__main__":
 
     # test save_first_layer_weight
     save_first_layer_weight(model, "../logs/first_layer_weight.png")
+
+    # test save_first_layer_weight_freq
+    save_first_layer_weight_freq(model, "../logs/first_layer_weight_freq.png")
